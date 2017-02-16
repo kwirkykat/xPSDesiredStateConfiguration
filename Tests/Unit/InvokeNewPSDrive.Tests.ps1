71ae307d7ae95eca8d3d26abf094329551e025b2
Katie Keim <kakeim@microsoft.com>
2017-02-15 17:36:31 -0800

    Test New-PSDrive mock

---
+++ Tests/Unit/InvokeNewPSDrive.Tests.ps1
@@ -0,0 +1,31 @@
+﻿function Invoke-NewPSDrive {
+    param ()
+
+    $testUserName = 'TestUsername'
+    $secureTestPassword = ConvertTo-SecureString -String 'F@kePassw0rd' -AsPlainText -Force
+
+    $testCredential = New-Object -TypeName 'System.Management.Automation.PSCredential' -ArgumentList @( $testUsername, $secureTestPassword )
+
+    $newPSDriveParameters = @{
+        Name = [System.Guid]::NewGuid()
+        PSProvider = 'FileSystem'
+        Root = $env:temp
+        Scope = 'Script'
+        Credential = $testCredential
+    }
+
+    $psDrive = New-PSDrive @newPSDriveParameters
+}
+
+Describe 'Invoke-NewPSDrive Test' {
+    $mockPSDrive = New-MockObject -Type 'System.Management.Automation.PSDriveInfo'
+    Mock -CommandName 'New-PSDrive' -MockWith { return $mockPSDrive }
+
+    It 'Should not throw' {
+        { Invoke-NewPSDrive } | Should Not Throw
+    }
+
+    It 'Should call New-PSDrive mock' {
+        Assert-MockCalled -CommandName 'New-PsDrive' -Exactly 1 -Scope 'Describe'
+    }
+}
